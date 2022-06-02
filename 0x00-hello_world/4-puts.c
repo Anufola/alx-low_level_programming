591c836b61d487b7b7a87bc5fc60fb58a7b9944b
Anuoluwapo <anufola8@gmail.com>
2022-06-02 05:17:44 -0700

    pr 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - print the string in the put function
  *
  * Description: using the function puts
- * The program prints "Programming is like building a multilingual puzzle"
+ * the program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  * */
 int main(void)
