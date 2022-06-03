05d9077b6bed07b755945701abcbab179d808f38
Anuoluwapo <anufola8@gmail.com>
2022-06-03 07:19:47 -0700

    fr

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
