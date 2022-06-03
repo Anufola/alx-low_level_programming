4c9390ca0d7b237006bcb9c77957fbe9a0e31912
Anuoluwapo <anufola8@gmail.com>
2022-06-03 07:02:18 -0700

    fh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -9,11 +8,15 @@
  */
 int main(void)
 {
-char low;
-for (low = 'a'; low <= 'z'; low++)
-putchar(low)
-for (low = 'A'; low <= 'Z'; low++)
-putchar(low);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
 putchar('\n');
 return (0);
 }
