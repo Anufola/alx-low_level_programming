a7d2285ad2a0102a51d959a247b4b27b4f771f23
Anuoluwapo <anufola8@gmail.com>
2022-06-03 07:34:04 -0700

    gh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
