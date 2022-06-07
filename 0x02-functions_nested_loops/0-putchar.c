cce3ff5fb10140edeca4bdca4bfb0dd1a62b8dd4
Anuoluwapo <anufola8@gmail.com>
2022-06-07 09:12:09 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,11 @@
 #include "main.h"
 int main.h(char c);
 {
-char msg[c] = "_putchar\n";
-int fd = open("/dev/tty", O_WRONLY);
-write(fd,c, msg, sizeof(msg));
- return 0;
+char word[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+write (word, i);
+_putchar(word[i]);
+_putchar('\n');
+return (0);
 }
