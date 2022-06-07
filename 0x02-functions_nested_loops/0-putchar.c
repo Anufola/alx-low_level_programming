18d4b54f8be6930f2d7e4a57befabd308c5855b3
Anuoluwapo <anufola8@gmail.com>
2022-06-07 09:02:45 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include "main.h"
-int _putchar(char c);
+int main.h(char c);
 {
-char msg[] = "_putchar\n";
+char msg[c] = "_putchar\n";
 int fd = open("/dev/tty", O_WRONLY);
-write(fd, msg, sizeof(msg));
+write(fd,c, msg, sizeof(msg));
  return 0;
 }
