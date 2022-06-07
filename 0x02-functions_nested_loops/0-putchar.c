cc390aa6e3f142b4095517ac477df6cf31b879d9
Anuoluwapo <anufola8@gmail.com>
2022-06-07 05:56:47 -0700

    j

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,11 @@
  *
  * Return: 0
  */
-int main(void)
-char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r',};
-unsigned int c;
-for (c = 0; c < sizeof(_putchar); c++)
-{
-_putchar(_putchar[c]);
-}
-_putchar('\n');
-return (0);
+void main() {
+int c;
+c = getchar();
+while (c != EOF) {
+putchar(c);
+c = getchar();
+return(0);}
 }
