6c0b9b5c04c4ae0d2c8d5d0d0ba2e58b43efb8c3
Anuoluwapo <anufola8@gmail.com>
2022-06-13 15:05:15 -0700

    hg

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-char *aux = dest;
+char *prt = dest;
+ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+printf("%s", s1);
+printf("%s", ptr);
 while (*src)
 *dest++ = *src++;
 return (aux);
